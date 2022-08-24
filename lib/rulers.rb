7c8fc9828e291229a506ee68a2a658d35bbf8ed8
David Stosik <davidstosik@users.noreply.github.com>
2022-08-24 20:20:22 +0200

    1. Exercise One: add a debug

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -5,6 +5,7 @@ require_relative "rulers/version"
 module Rulers
   class Application
     def call(_env)
+      `echo debug > debug.txt`
       [200, { "Content-Type" => "text/html" },
        ["Hello from Ruby on Rulers!"]]
     end
